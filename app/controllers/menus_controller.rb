1e62eb7aa9c4dfae6dedd9d83471995b49aa24a5
caraclarke <cara.clarke8@gmail.com>
2015-10-29 22:38:06 -0400

    Menu index working
    Index working for menu items

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,16 @@
+class MenusController < ApplicationController
+
+  def index
+    @menu = Menu.all
+    render json: @menu
+  end
+
+  private
+
+  def menu_params
+     params.require(:menu).permit(:course,
+                                  :title,
+                                  :description)
+  end
+
+end
